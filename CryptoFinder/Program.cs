bd9dab14e22e9b9b500a926d1f98f100704329e4
Hyper_ <83446248+Purcify92@users.noreply.github.com>
2023-10-05 08:25:12 +1000

    Initial commit

---
+++ CryptoFinder/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json; // Make sure to include this namespace for JsonConvert
+
+namespace CryptoFinder
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Title = "CryptoFinder by Purcify";
+            Console.WriteLine("What crypto would you like to find information about?");
+            string reply = Console.ReadLine().ToLower();
+
+            using var client = new HttpClient(); // Use 'using' to properly dispose of HttpClient
+            var response = await client.GetAsync("https://api.coincap.io/v2/assets/" + reply);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonContent = await response.Content.ReadAsStringAsync();
+                var jsonObject = JsonConvert.DeserializeObject<ApiResponse>(jsonContent);
+
+                if (jsonObject != null && jsonObject.Data != null)
+                {
+                    var price = jsonObject.Data.PriceUsd;
+                    Console.WriteLine($"Current price of {reply}: ${price}");
+                }
+                else
+                {
+                    Console.WriteLine("Failed to parse JSON response.");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode}");
+            }
+        }
+    }
+
+    public class ApiResponse
+    {
+        public Data Data { get; set; }
+    }
+
+    public class Data
+    {
+        [JsonProperty("priceUsd")]
+        public decimal PriceUsd { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CryptoFinder
+ An easy C# console app for finding current crypto prices.
